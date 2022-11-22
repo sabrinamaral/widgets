90cef34d24ec8332652b0851221e6a764a97546b
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-22 19:05:23 -0300

    Accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [expendedIndex, setExpendedIndex] = useState(-1);
+  const handleClick = (nextIndex) => {
+    if (expendedIndex === nextIndex) {
+      setExpendedIndex(-1);
+    } else {
+      setExpendedIndex(nextIndex);
+    }
+  };
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expendedIndex;
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronRight />}
+      </span>
+    );
+
     return (
-      <React.Fragment key={item.title}>
-        <div className="title active">
-          <i className="dropdown icon"></i>
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
           {item.title}
+          {icon}
         </div>
-        <div className="content active">
-          <p>{item.content}</p>
-        </div>
-      </React.Fragment>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 };
 
 export default Accordion;
