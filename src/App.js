df8d0383ff3be85c03c127a9ecb1bbc76bcb8122
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-26 09:00:28 -0300

    App update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { useState } from "react";
-import DropDown from "./components/DropDown";
-
 const App = () => {
-  const [selected, setSelected] = useState(null);
-  const handleSelect = (option) => {
-    setSelected(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <DropDown options={options} onChange={handleSelect} value={selected} />
-  );
+  return <div>App</div>;
 };
 
 export default App;
