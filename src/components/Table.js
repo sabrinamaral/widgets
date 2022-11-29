8803771216d8a0eabd497c9f4b3c2729b3175d29
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-28 21:55:43 -0300

    better styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,19 @@
 const Table = ({ data }) => {
   const renderRows = data.map(({ name, color, score }) => {
     return (
-      <tr key={name}>
-        <td>{name}</td>
-        <td className={color}></td>
-        <td>{score}</td>
+      <tr className="border-b" key={name}>
+        <td className="p-3">{name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${color}`}></div>
+        </td>
+        <td className="p-3">{score}</td>
       </tr>
     );
   });
   return (
-    <table>
+    <table className="table-auto border-spacing-2">
       <thead>
-        <tr>
+        <tr className="border-b-2">
           <th>Fruit</th>
           <th>Color</th>
           <th>Score</th>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -2,8 +2,8 @@ import Table from "../components/Table";
 const data = [
   { name: "Orange", color: "bg-orange-500", score: 5 },
   { name: "Apple", color: "bg-red-500", score: 3 },
-  { name: "Banana", color: "bg-yellow-500", score: 1 },
-  { name: "Lime", color: "bg-green-500", score: 4 },
+  { name: "Banana", color: "bg-yellow-400", score: 1 },
+  { name: "Lime", color: "bg-green-600", score: 4 },
 ];
 const TablePage = () => {
   return (
