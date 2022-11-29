86f91f0563aed203ac8c5b098314152a9b5dd7b7
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-28 21:40:27 -0300

    table structure

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,25 @@
-const Table = () => {
-  return <div>Table</div>;
+const Table = ({ data }) => {
+  const renderRows = data.map(({ name, color, score }) => {
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td className={color}></td>
+        <td>{score}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows}</tbody>
+    </table>
+  );
 };
 
 export default Table;
