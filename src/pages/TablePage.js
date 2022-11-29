929604e2d546a8b6c2c99ba097ecb78f5f1d4ffa
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-28 21:40:36 -0300

    table data

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,14 @@
 import Table from "../components/Table";
-
+const data = [
+  { name: "Orange", color: "bg-orange-500", score: 5 },
+  { name: "Apple", color: "bg-red-500", score: 3 },
+  { name: "Banana", color: "bg-yellow-500", score: 1 },
+  { name: "Lime", color: "bg-green-500", score: 4 },
+];
 const TablePage = () => {
   return (
     <div>
-      <Table />
+      <Table data={data} />
     </div>
   );
 };
