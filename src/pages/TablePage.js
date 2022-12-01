90469f3e9735652edb82463683836c2680348fea
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-30 21:58:17 -0300

    customizing headers

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,8 +1,8 @@
 import Table from "../components/Table";
 
 const data = [
-  { name: "Orange", color: "bg-orange-500", score: 5 },
-  { name: "Apple", color: "bg-red-500", score: 3 },
+  { name: "Apple", color: "bg-red-500", score: 5 },
+  { name: "Orange", color: "bg-orange-500", score: 3 },
   { name: "Banana", color: "bg-yellow-400", score: 1 },
   { name: "Lime", color: "bg-green-600", score: 4 },
 ];
@@ -13,9 +13,12 @@ const TablePage = () => {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="text-red-500">Score</th>,
+    },
   ];
-
   const keyFn = (fruit) => {
     return fruit.name;
   };
